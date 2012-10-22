941de3a5d232383eaee22715c3b145561dd3d5a7
Alexey Kupershtokh <alexey.kupershtokh@gmail.com>
2012-10-22 17:12:39 +0700

    use perf-time module in a core for more precision

--- perf-timers.js
+++ perf-timers.js
@@ -1,3 +1,6 @@
+var perfTime = require('perf-time');
+var time = new perfTime();
+
 module.exports = PerfTimers = function(handler_or_interval, interval) {
 	if (typeof handler_or_interval == 'function') {
 		this.handler = handler_or_interval;
@@ -8,18 +11,17 @@ module.exports = PerfTimers = function(handler_or_interval, interval) {
 	}
     this.timers = {};
     this.i = null;
-    this.now = Date.now();
 	this.__iterate = this._iterate.bind(this);
 };
 PerfTimers.prototype.setTimeout = function(cb, timeout) {
-    var at = this.now + this.interval + timeout;
+    var at = time.get() + this.interval + timeout;
     at -= at % this.interval;
     var t = this.timers[at] || (t = this.timers[at] = []);
     t.push(cb);
 	this.i = this.i || setInterval(this.__iterate, this.interval);
 };
 PerfTimers.prototype._iterate = function() {
-    var n = this.now = Date.now();
+    var n = time.get();
     for (var at in this.timers) if (at < n) {
 		var t = this.timers[at], l = t.length;
 		delete this.timers[at];
