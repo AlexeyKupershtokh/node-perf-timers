b8d54ed6399fa3ea753e34860671bc6254fa0459
Alexey Kupershtokh <alexey.kupershtokh@gmail.com>
2012-10-22 17:14:59 +0700

    improved benchmarks

--- bench/perf-timers-handler.js
+++ bench/perf-timers-handler.js
@@ -7,12 +7,12 @@ var handler = function(p) {
     for (var j = 0, l = p.length; j < l; j++) {
         x++;
 		var i = p[j];
-		t.setTimeout(i, i % 5000 + 1);
+		t.setTimeout(i, i % 1000 + 1);
 	}
 };
-var t = new perftimers(handler, 1);
-for (var i = 0; i < 5000000; i++) {
-	t.setTimeout(i, i % 5000 + 1);
+var t = new perftimers(handler, 10);
+for (var i = 0; i < 1000000; i++) {
+	t.setTimeout(i, i % 1000 + 1);
 }
 
 setInterval(function() {
